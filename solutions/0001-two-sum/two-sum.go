40062dba670fa3183f5dbade69ab6dabd3529670
cladamos <caliskangokberk@gmail.com>
2026-09-21 22:51:53 +0300

    Add solution 0001

---
+++ solutions/0001-two-sum/two-sum.go
@@ -0,0 +1,12 @@
+package solutions
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, num := range nums {
+		if v, ok := m[target-num]; ok {
+			return []int{v, i}
+		}
+		m[num] = i
+	}
+	return nil
+}
